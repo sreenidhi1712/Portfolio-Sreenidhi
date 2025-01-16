d5987ff54a2d43eda713fabd7a76a96d5d043b2d
[Sreenidhi D] <[sreeindra01@gmail.com]>
2025-01-16 10:08:58 +0530

    add emailjs

--- app/Components/ContactMeComponent/Contactme.jsx
+++ app/Components/ContactMeComponent/Contactme.jsx
@@ -5,10 +5,15 @@ import { MdOutlineEmail } from "react-icons/md";
 import { FaPhoneAlt } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
+import { useState } from 'react';
+import emailjs from 'emailjs-com';
 
 const linkedinUrl = process.env.NEXT_PUBLIC_LINKEDIN_URL;
 const githubUrl = process.env.NEXT_PUBLIC_GITHUB_URL;
 const resumeUrl = process.env.NEXT_PUBLIC_RESUME_URL;
+const serviceID = process.env.NEXT_PUBLIC_SERVICE_ID;
+const templateID = process.env.NEXT_PUBLIC_TEMPLATE_ID;
+const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY;
 
 const contacts = [
   {
@@ -27,11 +32,53 @@ const contacts = [
 
 function Contactme() {
 
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    message: "",
+  });
 
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
   const handleLinks = (links)=>{
     window.open(links, '_blank');
   }
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .send(
+        serviceID, // Replace with your EmailJS Service ID
+        templateID, // Replace with your EmailJS Template ID
+        {
+          from_name: `${formData.firstName}  ${formData.lastName}`,
+          from_email: formData.email,
+          message: formData.message,
+        },
+        publicKey// Replace with your EmailJS Public Key
+      )
+      .then(
+        (response) => {
+          console.log("Message sent successfully!");
+          setFormData({
+            firstName: "",
+            lastName: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          console.log("Failed to send the message. Please try again.");
+        }
+      );
+  };
+
+
   return (
     <div className={styles.Contactcontainer}>
              <div className={styles.ContactTitle}>
@@ -62,24 +109,29 @@ function Contactme() {
            Help me out with your Feedback or Referrals
         </p>
       </div>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         {/* Name Fields */}
         <div className={styles.row}>
-          <input type="text" placeholder="First Name" className={styles.input} />
-          <input type="text" placeholder="Last Name" className={styles.input} />
+          <input type="text" placeholder="First Name" name='firstName' value={formData.firstName} onChange={handleChange} className={styles.input} />
+          <input type="text" placeholder="Last Name" name='lastName' value={formData.lastName} onChange={handleChange} className={styles.input} />
         </div>
         {/* Email and Subject Fields */}
         <div className={styles.row}>
           <input
             type="email"
+            name='email'
             placeholder="Email Address"
+            value={formData.email}
+            onChange={handleChange}
             className={styles.input}
           />
-          <input type="text" placeholder="Subject" className={styles.input} />
         </div>
         {/* Message Field */}
         <textarea
           placeholder="Your Message"
+          name='message'
+          value={formData.message}
+          onChange={handleChange}
           className={styles.textarea}
         ></textarea>
         {/* Submit Button */}
